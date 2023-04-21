2aea854df712e15e05b2c5ed0f6089d1c6966e09
Marcus Worrell <129471786+judahwo@users.noreply.github.com>
2023-04-20 21:53:11 -0500

    notes

--- Animate_Robot.py
+++ Animate_Robot.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # This program employs forward kinematics for a 4R Robot in 3D
-# The function, forward_kinematics_4R, is passed link lengths and joint angles, and returns joint and end effectors
-# positions. The program plots the links, joint and end effector positions.
+# The function, forward_kinematics_4R, is passed link lengths and joint angles, and returns joints and end effector
+# positions. The program plots and animates the motion of the robot arm.
 
 # Define link lengths
 L1 = 25
@@ -12,10 +12,10 @@ L3 = 95
 L4 = 80
 L5 = 25
 
-# Your given function
+# Forward kinematics main function
 def forward_kinematics_4R(t1, t2, t3, t4, L1, L2, L3, L4, L5):
 
-    # Frame to frame transformation matrices
+    # Define the homogenous transformation matrices
     T01 = np.array([[np.cos(t1), 0, np.sin(t1), 0],
                    [np.sin(t1), 0, -np.cos(t1), 0],
                    [0, 1, 0, L1],
@@ -41,13 +41,13 @@ def forward_kinematics_4R(t1, t2, t3, t4, L1, L2, L3, L4, L5):
                    [0, 0, 1, 0],
                    [0, 0, 0, 1]])
 
-    # Joint and EE homogenous transformation matrices
+    # Calculate the transformation matrices
     T02 = np.dot(T01, T12)
     T03 = np.dot(T02, T23)
     T04 = np.dot(T03, T34)
     T05 = np.dot(T04, T45)
 
-    # Extract joint and EE positions from transformation matrices
+    # Calculate the end effector and joint positions
     posee = T05[:3,3]
     pos1 = T01[:3,3]
     pos2 = T02[:3,3]
@@ -56,11 +56,11 @@ def forward_kinematics_4R(t1, t2, t3, t4, L1, L2, L3, L4, L5):
 
     return posee, pos1, pos2, pos3, pos4
 
-# Linear interpolation
+# Define a function to interpolate between joint angles
 def interpolate_angles(start_angles, end_angles, steps):
     return np.linspace(start_angles, end_angles, steps)
 
-# Define joint angles for each pose
+# Define the joint angles for each pose
 pose1_angles = np.array([0, 2.1204, -0.8430, -1.508])
 pose2_angles = np.array([1.5, 2.1204, -0.8430, -1.508])
 pose3_angles = np.array([1.5, 1.1204, -1.2430, -1.508])
@@ -69,10 +69,10 @@ pose5_angles = np.array([-1.5, 1.5204, -0.8430, -1.508])
 pose6_angles = np.array([-1.5, 1.1204, -1.2430, -1.508])
 pose7_angles = np.array([0, 2.1204, -0.8430, -1.508])
 
-# Define the number of steps for interpolation
+# Define the number of steps to interpolate between poses
 steps = 30
 
-# Interpolate between poses
+# Interpolate between poses (function call)
 path1 = interpolate_angles(pose1_angles, pose2_angles, steps)
 path2 = interpolate_angles(pose2_angles, pose3_angles, steps)
 path3 = interpolate_angles(pose3_angles, pose4_angles, steps)
@@ -80,27 +80,27 @@ path4 = interpolate_angles(pose4_angles, pose5_angles, steps)
 path5 = interpolate_angles(pose5_angles, pose6_angles, steps)
 path6 = interpolate_angles(pose6_angles, pose7_angles, steps)
 
-# Combine paths
+# Concatenate the paths
 path = np.concatenate((path1, path2, path3, path4, path5, path6), axis=0)
 
-# Animation plot setup
+# Create a figure and 3D axes
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# Set the initial viewing position
+# Set the viewing angle
 ax.view_init(elev=25, azim=-45)
 
-# Adjust the height, width, and aspect ratio of the viewing window
+# Set the aspect ratio of the plot, adjust the margins, and size of the window
 ax.set_box_aspect([1, 1, 0.7])  # Set the aspect ratio of the plot
 fig.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)  # Adjust the margins of the plot
 fig.set_size_inches(8, 8)  # Set the size of the window in inches
 
-# Animate the motion
+# Animate the arm
 for angles in path:
     t1, t2, t3, t4 = angles
-    posee, pos1, pos2, pos3, pos4 = forward_kinematics_4R(t1, t2, t3, t4, L1, L2, L3, L4, L5)
+    posee, pos1, pos2, pos3, pos4 = forward_kinematics_4R(t1, t2, t3, t4, L1, L2, L3, L4, L5) # Function call
 
-    # Titles, labels, and limits
+    # Axes labels, title, and limits
     ax.clear()
     ax.set_xlabel('X-Axi (mm)')
     ax.set_ylabel('Y-Axis (mm))')
@@ -110,7 +110,7 @@ for angles in path:
     ax.set_ylim([-200, 200])
     ax.set_zlim([-10, 300])
 
-    # Plot the arm's links
+    # Plot the arm
     ax.plot([0, pos1[0], pos2[0], pos3[0], pos4[0], posee[0]],
             [0, pos1[1], pos2[1], pos3[1], pos4[1], posee[1]],
             [0, pos1[2], pos2[2], pos3[2], pos4[2], posee[2]], 
@@ -118,7 +118,8 @@ for angles in path:
              markerfacecolor='red', markeredgecolor='black',
              linewidth=6, markeredgewidth=1, color='black')
 
-    plt.draw()
+    # Plot the end effector
+    plt.draw()  
     plt.pause(0.01)
 
 plt.show()
